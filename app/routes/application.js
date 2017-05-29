190857227d2c31681f224496676a70b523ad1541
CosmicCoder96 <abhinav.khare31@gmail.com>
2017-05-30 01:36:24 +0530

    Feat : Learned how to extend ember object class

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,17 +2,22 @@ import Ember from 'ember';
 
 const { Route } = Ember;
 
+var Song = Ember.Object.extend({
+  title: '',
+  band: '',
+  rating: 0
+});
 export default Route.extend({
   model: function() {
-    var blackDog = Ember.Object.create({ title: 'Black Dog',
+    var blackDog = Song.create({ title: 'Black Dog',
       band: 'Led Zeppelin',
       rating: 3
     });
-    var yellowLedbetter = Ember.Object.create({ title: 'Yellow Ledbetter',
+    var yellowLedbetter = Song.create({ title: 'Yellow Ledbetter',
       band: 'Pearl Jam',
       rating: 4
     });
-    var pretender = Ember.Object.create({ title: 'The Pretender',
+    var pretender = Song.create({ title: 'The Pretender',
       band: 'Foo Fighters',
       rating: 2
     });
