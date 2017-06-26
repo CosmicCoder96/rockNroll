ff1865520d036e49280e37c2affea57b15a9beba
CosmicCoder96 <abhinav.khare31@gmail.com>
2017-06-26 07:07:12 +0530

    Add warning to leave description unsaved

--- app/controllers/bands/band/details.js
+++ app/controllers/bands/band/details.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
-export default Ember.Controller.extend({
+const { Controller } = Ember;
+export default Controller.extend({
+  isEditing : false,
+  actions   : {
+    edit() {
+      this.set('isEditing', true);
+    },
+    save() {
+      this.set('isEditing', false);
+    }
+  }
 });
--- app/routes/bands/band/details.js
+++ app/routes/bands/band/details.js
@@ -1,9 +1,23 @@
 import Ember from 'ember';
 
-const { Route } = Ember;
+const {Route} = Ember;
 
 export default Route.extend({
   model() {
     return this.modelFor('bands.band');
+  },
+  actions: {
+    willTransition(transition) {
+      var controller = this.get('controller'), leave;
+      if (controller.get('isEditing')) {
+        leave = window.confirm('You have unsaved changes. Are you sure you want to leave?');
+        if (leave) {
+          controller.set('isEditing', false);
+        }
+        else {
+          transition.abort();
+        }
+      }
+    }
   }
 });
