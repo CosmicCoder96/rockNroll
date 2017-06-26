bc9cd0ff0ef72723abe1cb128cb145da2a8a4bbb
CosmicCoder96 <abhinav.khare31@gmail.com>
2017-06-26 06:53:39 +0530

    Fix bug which caused new song nudge failure

--- app/routes/bands/band/songs.js
+++ app/routes/bands/band/songs.js
@@ -1,12 +1,15 @@
 import Ember from 'ember';
 import Song from '../../../models/song';
-export default Ember.Route.extend({
-  model: function() {
+
+const { Route } = Ember;
+
+export default Route.extend({
+  model() {
     return this.modelFor('bands.band');
   },
 
   actions: {
-    createSong: function() {
+    createSong() {
       var controller = this.get('controller');
       var band = this.modelFor('bands.band');
       var title = controller.get('title');
@@ -15,5 +18,9 @@ export default Ember.Route.extend({
       band.get('songs').pushObject(song);
       controller.set('title', '');
     }
+  },
+
+  resetController(controller) {
+    controller.set('songCreationStarted', false);
   }
-})
+});
