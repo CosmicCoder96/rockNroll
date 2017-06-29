aae13d452b3cd38ef0a8c93c726dcb583d360a86
CosmicCoder96 <abhinav.khare31@gmail.com>
2017-06-30 04:00:08 +0530

    Get rid of slug property in band

--- app/models/band.js
+++ app/models/band.js
@@ -1,15 +1,10 @@
-import Ember from 'ember';
 import DS from 'ember-data';
 
-const { computed } = Ember;
 const { Model, attr, hasMany } = DS;
 
 export default Model.extend(
   {
     name        : attr('string'),
     description : attr(),
-    songs       : hasMany('song'),
-    slug        : computed('name', function () {
-      return this.get('name').dasherize();
-    })
+    songs       : hasMany('song')
   });
