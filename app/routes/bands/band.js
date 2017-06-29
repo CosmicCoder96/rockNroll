65401f6cbd99a35a6814d16b2063b8f5dc0d4be6
CosmicCoder96 <abhinav.khare31@gmail.com>
2017-06-30 03:58:30 +0530

    API: modify band route

--- app/routes/bands/band.js
+++ app/routes/bands/band.js
@@ -4,8 +4,6 @@ const { Route } = Ember;
 
 export default Route.extend({
   model(params) {
-    this.set('params', params);
-    var bands = this.modelFor('bands');
-    return bands.findBy('slug', params.slug);
+    return this.store.findRecord('band', params.id);
   }
 });
